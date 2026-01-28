c547ea44c67557094137d5631c7815a0ffb29332
kamruzzaman-insights <kamruzzaman.insights@gmail.com>
2026-01-28 19:13:25 +0600

    dynamic-login-complete

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("account-number").value;
+
+    const password = document.getElementById("password").value;
+    const convertPassword = parseInt(password);
+
+    if( accountNumber === '01518759172' ) {
+        if(convertPassword === 1234) {
+            window.location.href = './main.html';
+        }
+        else{
+            alert('invalid password');
+        }
+    } 
+    else{
+        alert('invalid number');
+    }
+})
