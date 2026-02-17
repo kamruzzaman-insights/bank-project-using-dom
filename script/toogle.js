55007adbbc402a054ab26e3924d63b30c83039a4
kamruzzaman-insights <kamruzzaman.insights@gmail.com>
2026-02-17 19:17:47 +0600

    efficiant-toggle-code

--- script/toogle.js
+++ script/toogle.js
@@ -7,24 +7,43 @@ const sections = [
     "transaction-history-section"
 ];
 
-document.getElementById('add-money-btn').addEventListener("click", function () {
-    showSection("add-money-section");
-})
-document.getElementById('cash-out-btn').addEventListener("click", function () {
-    showSection("cash-out-section");
-})
-document.getElementById('transfer-money-btn').addEventListener("click", function () {
-    showSection("transfer-money-section");
-})
-document.getElementById('get-bonus-btn').addEventListener("click", function () {
-    showSection("get-bonus-section");
-})
-document.getElementById('pay-bill-btn').addEventListener("click", function () {
-    showSection("pay-bill-section");
-})
-document.getElementById('transaction-history-btn').addEventListener("click", function () {
-    showSection("transaction-history-section");
-})
+const buttons = {
+    "add-money-btn": "add-money-section",
+    "cash-out-btn": "cash-out-section",
+    "transfer-money-btn": "transfer-money-section",
+    "get-bonus-btn": "get-bonus-section",
+    "pay-bill-btn": "pay-bill-section",
+    "transaction-history-btn": "transaction-history-section"
+}
+
+
+
+
+Object.keys(buttons).forEach(btnId => {
+  document.getElementById(btnId).addEventListener("click", () => {
+    showSection(buttons[btnId]);
+  });
+});
+
+
+// document.getElementById('add-money-btn').addEventListener("click", function () {
+//     showSection("add-money-section");
+// })
+// document.getElementById('cash-out-btn').addEventListener("click", function () {
+//     showSection("cash-out-section");
+// })
+// document.getElementById('transfer-money-btn').addEventListener("click", function () {
+//     showSection("transfer-money-section");
+// })
+// document.getElementById('get-bonus-btn').addEventListener("click", function () {
+//     showSection("get-bonus-section");
+// })
+// document.getElementById('pay-bill-btn').addEventListener("click", function () {
+//     showSection("pay-bill-section");
+// })
+// document.getElementById('transaction-history-btn').addEventListener("click", function () {
+//     showSection("transaction-history-section");
+// })
 
 function showSection(activeId) {
     sections.forEach(id => {
@@ -32,5 +51,13 @@ function showSection(activeId) {
     })
     document.getElementById(activeId).style.display = "block";
 }
+
+function hideAllSection () {
+    sections.forEach(id => {
+        document.getElementById(id).style.display = "none";
+    });
+}
+
+hideAllSection();
 
 
